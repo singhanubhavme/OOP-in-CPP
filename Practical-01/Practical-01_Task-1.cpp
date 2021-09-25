4ef5dc5ad08a8264747fdfdd8dbc76b67547e739
Anubhav Singh <Anubhav>
2021-09-26 00:57:53 +0530

    first commit

---
+++ Practical-01/Practical-01_Task-1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define NUM 7
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << NUM << " * " << i << " = " << NUM * i << endl;
+    }
+}
