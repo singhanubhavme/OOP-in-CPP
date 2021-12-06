c25fc90448bb2b9b9c97ccd88a5ff0dfe955576b
Anubhav Singh <80422816+singhanubhavme@users.noreply.github.com>
2021-12-06 10:31:31 +0530

    Add files via upload

---
+++ Practical-11/Practical-11_Task-2.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+int x;
+protected:
+int y;
+private:
+int z;
+};
+class B:public A
+{
+public:
+void display(){
+x = 10, y = 20;
+// z = 30; // error
+cout << x <<" " << y << " " << endl;
+}
+};
+int main()
+{
+B obj;
+obj.x = 11;
+//obj.y = 12;
+//obj.z = 13;
+obj.display();
+return 0;
+}
+
